433d66f67bb2aa3d9323aa7706efc81190d21aaf
Yuktha Raman G V <yuktha.r.gv@kipi.bi>
2023-08-22 15:02:25 +0530

    wk

--- kipi_watchkeeper.tf
+++ kipi_watchkeeper.tf
@@ -19,7 +19,7 @@ resource "snowflake_resource_monitor" "watchkeeper-monitor" {
     credit_quota = 15
 
     frequency       = "MONTHLY"
-    start_timestamp = "22-08-2023"
+    start_timestamp = "2023-08-22 00:00"
 
     notify_triggers           = [50,60]
     suspend_trigger           = 70
