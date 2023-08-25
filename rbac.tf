2582a4b9fe120d114e5c24d85d9320bbeaf647b6
Yuktha Raman G V <yuktha.r.gv@kipi.bi>
2023-08-25 14:12:42 +0530

    rbac dev roles + watchkeeper grant changes

--- rbac.tf
+++ rbac.tf
@@ -1,7 +1,75 @@
+//DEV ROLES
 resource "snowflake_role" "AMBYINT_DEV_READ_role" {
   name    = "AMBYINT_DEV_READ"
   comment = "For Ambyint DEV Read"
 }
+
+resource "snowflake_role" "DEV_READ_role" {
+  name    = "DEV_READ"
+}
+
+resource "snowflake_role_grants" "grant-dev-access-role" {
+  role_name = snowflake_role.DEV_READ_role.name
+
+  roles = [
+    snowflake_role.AMBYINT_DEV_READ_role.name
+  ]
+}
+
+resource "snowflake_role_grants" "grant-dev-func-role" {
+  role_name = snowflake_role.AMBYINT_DEV_READ_role.name
+
+  roles = [
+    "SYSADMIN"
+  ]
+
+  users = ["TERRAFORM"
+  ]
+}
+
+resource "snowflake_role" "DEV_AMBYINT_DS_SERVICE_role" {
+  name    = "DEV_AMBYINT_DS_SERVICE"
+  comment = "For Ambyint DS Service DEV Read"
+}
+
+resource "snowflake_role_grants" "grant-dev-ds-role" {
+  role_name = snowflake_role.DEV_AMBYINT_DS_SERVICE_role.name
+
+  roles = [
+    "SYSADMIN"
+  ]
+
+  users = ["TERRAFORM"
+  ]
+}
+
+resource "snowflake_role" "DEV_DS_RO_role" {
+  name    = "DEV_DS_RO"
+}
+
+resource "snowflake_role_grants" "grant-dev-ds-ro" {
+  role_name = snowflake_role.DEV_DS_RO_role.name
+
+  roles = [
+    snowflake_role.DEV_AMBYINT_DS_SERVICE_role.name
+  ]
+}
+
+
+resource "snowflake_role" "DEV_DS_WRITE_role" {
+  name    = "DEV_DS_WRITE"
+}
+
+resource "snowflake_role_grants" "grant-dev-ds-rw" {
+  role_name = snowflake_role.DEV_DS_WRITE_role.name
+
+  roles = [
+    snowflake_role.DEV_AMBYINT_DS_SERVICE_role.name
+  ]
+}
+
+
+
 
 resource "snowflake_role" "AMBYINT_QA_READ_role" {
   name    = "AMBYINT_QA_READ"
@@ -14,9 +82,6 @@ resource "snowflake_role" "AMBYINT_PROD_READ_role" {
 }
 
 
-resource "snowflake_role" "DEV_READ_role" {
-  name    = "DEV_READ"
-}
 
 
 resource "snowflake_role" "QA_READ_role" {
@@ -28,13 +93,7 @@ resource "snowflake_role" "PROD_READ_role" {
 }
 
 
-resource "snowflake_role_grants" "grant-dev-access-role" {
-  role_name = snowflake_role.DEV_READ_role.name
 
-  roles = [
-    snowflake_role.AMBYINT_DEV_READ_role.name
-  ]
-}
 
 resource "snowflake_role_grants" "grant-qa-access-role" {
   role_name = snowflake_role.QA_READ_role.name
@@ -51,17 +110,6 @@ resource "snowflake_role_grants" "grant-prod-access-role" {
   ]
 }
 
-
-resource "snowflake_role_grants" "grant-dev-func-role" {
-  role_name = snowflake_role.AMBYINT_DEV_READ_role.name
-
-  roles = [
-    "SYSADMIN"
-  ]
-
-  users = ["TERRAFORM"
-  ]
-}
 
 
 resource "snowflake_role_grants" "grant-qa-func-role" {
@@ -88,21 +136,7 @@ resource "snowflake_role_grants" "grant-prod-func-role" {
 }
 
 
-resource "snowflake_role" "DEV_AMBYINT_DS_SERVICE_role" {
-  name    = "DEV_AMBYINT_DS_SERVICE"
-  comment = "For Ambyint DS Service DEV Read"
-}
 
-resource "snowflake_role_grants" "grant-dev-ds-role" {
-  role_name = snowflake_role.DEV_AMBYINT_DS_SERVICE_role.name
-
-  roles = [
-    "SYSADMIN"
-  ]
-
-  users = ["TERRAFORM"
-  ]
-}
 
 resource "snowflake_role" "QA_AMBYINT_DS_SERVICE_role" {
   name    = "QA_AMBYINT_DS_SERVICE"
@@ -136,30 +170,7 @@ resource "snowflake_role_grants" "grant-prod-ds-role" {
 
 
 
-resource "snowflake_role" "DEV_DS_RO_role" {
-  name    = "DEV_DS_RO"
-}
 
-resource "snowflake_role_grants" "grant-dev-ds-ro" {
-  role_name = snowflake_role.DEV_DS_RO_role.name
-
-  roles = [
-    snowflake_role.DEV_AMBYINT_DS_SERVICE_role.name
-  ]
-}
-
-
-resource "snowflake_role" "DEV_DS_WRITE_role" {
-  name    = "DEV_DS_WRITE"
-}
-
-resource "snowflake_role_grants" "grant-dev-ds-rw" {
-  role_name = snowflake_role.DEV_DS_WRITE_role.name
-
-  roles = [
-    snowflake_role.DEV_AMBYINT_DS_SERVICE_role.name
-  ]
-}
 
 resource "snowflake_role" "QA_DS_RO_role" {
   name    = "QA_DS_RO"
@@ -207,3 +218,9 @@ resource "snowflake_role_grants" "grant-prod-ds-rw" {
   ]
 }
 
+resource "snowflake_schema" "grant-check" {
+  database_name = "MONITOR_DB"
+  schema_name="GRANT_CHECK"
+  privilege="USAGE"
+  roles=["PROD_DS_RO"]
+}
